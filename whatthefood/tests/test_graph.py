Convolution op, tests, some fixes and tests for gradient with placeholders

whatthefood/tests/test_graph.py
@@ -1,7 +1,6 @@
 import unittest
 import numpy as np
 
-from whatthefood.graph import run
 import whatthefood.graph as graph
 
 
@@ -9,36 +8,36 @@ class TestGraph(unittest.TestCase):
     def test_simple(self):
         c = graph.Constant(2)
 
-        self.assertEqual(run(c), 2)
+        self.assertEqual(graph.run(c), 2)
 
     def test_multiple_simple(self):
         c1 = graph.Constant(2)
         c2 = graph.Constant(3)
 
-        self.assertSequenceEqual(run((c1, c2)), (2, 3))
+        self.assertSequenceEqual(graph.run((c1, c2)), (2, 3))
 
     def test_non_trivial(self):
         a = graph.Constant(2)
         b = graph.Constant(3)
         c = graph.Sum(a, b)
 
-        self.assertEqual(run(c), 5)
+        self.assertEqual(graph.run(c), 5)
 
     def test_placeholder(self):
         a = graph.Placeholder(shape=(), batched=False)
         b = graph.Constant(2)
         c = graph.Sum(a, b)
 
-        self.assertEqual(run(c, {a: 3}), 5)
-        self.assertEqual(run(c, {a: 4}), 6)
+        self.assertEqual(graph.run(c, {a: 3}), 5)
+        self.assertEqual(graph.run(c, {a: 4}), 6)
 
     def test_batched(self):
         a = graph.Placeholder(shape=(2,), batched=True)
         b = graph.Constant([1, 2])
         c = graph.Sum(a, b)
 
-        np.testing.assert_array_equal(run(c, {a: np.array([[1, 2]])}), np.array([[2, 4]]))
-        np.testing.assert_array_equal(run(c, {a: np.array([[1, 2], [3, 4]])}), np.array([[2, 4], [4, 6]]))
+        np.testing.assert_array_equal(graph.run(c, {a: np.array([[1, 2]])}), np.array([[2, 4]]))
+        np.testing.assert_array_equal(graph.run(c, {a: np.array([[1, 2], [3, 4]])}), np.array([[2, 4], [4, 6]]))
 
     def test_grad_simple(self):
         a = graph.Constant(1)
@@ -50,7 +49,21 @@ class TestGraph(unittest.TestCase):
 
         g = graph.Grad(e, [a, b, c])
 
-        self.assertSequenceEqual(run(g), [4, 4, 3])
+        self.assertSequenceEqual(graph.run(g), [4, 4, 3])
+
+    def test_grad_placeholder_variable(self):
+        a = graph.Constant(1)
+        b = graph.Placeholder(batched=False, shape=())
+        c = graph.Variable(shape=())
+
+        c.value = 4
+
+        d = graph.Sum(a, b)
+        e = graph.MultiplyByScalar(d, c)
+
+        g = graph.Grad(e, [a, b, c])
+
+        self.assertSequenceEqual(graph.run(g, {b: 2}), [4, 4, 3])
 
     def test_grad(self):
         a = graph.Constant(1)
@@ -63,5 +76,5 @@ class TestGraph(unittest.TestCase):
 
         g = graph.Grad(f, [a, b, c])
 
-        self.assertSequenceEqual(run(g), [6, 9, 3])
+        self.assertSequenceEqual(graph.run(g), [6, 9, 3])
 