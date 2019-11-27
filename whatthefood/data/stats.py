61b30568b5f4a530923310bb85f318fd22242e0c
lychanl <lycha_@poczta.onet.pl>
2019-11-27 16:08:52 +0100

    Annotation loading and statistics tools

---
+++ whatthefood/data/stats.py
@@ -0,0 +1,83 @@
+import itertools
+import sys
+
+import numpy as np
+
+from whatthefood.data.xml_to_obj import parse_dir
+
+
+def dir_stats(dir_name):
+    data = parse_dir(dir_name)
+
+    to_stat = {
+        'Image dimensions': [a.img_size for a in data],
+        'Objects number': [len(a.objects) for a in data],
+        'Minimal bounding box distances': get_min_bb_dists(data),
+        'Minimal bounding box distances (different labels)': get_min_bb_dists(data, False),
+        'Bounding box centers': [o.center for a in data for o in a.objects],
+        'Bounding box sizes': [o.size for a in data for o in a.objects],
+    }
+
+    stats = {
+        'Mean': lambda x: np.mean(x, axis=0),
+        'Min': lambda x: np.min(x, axis=0),
+        'Max': lambda x: np.max(x, axis=0),
+        'Q1': lambda x: np.quantile(x, q=0.25, axis=0),
+        'Q2': lambda x: np.quantile(x, q=0.5, axis=0),
+        'Q3': lambda x: np.quantile(x, q=0.75, axis=0),
+    }
+
+    label_occurences = count_label_occurrences(data)
+
+    return {
+        prop_k: {
+            stat_k: stat_f(prop_v)
+            for stat_k, stat_f in stats.items()
+        }
+        for prop_k, prop_v in to_stat.items()
+    }, label_occurences
+
+
+def get_min_bb_dists(data, count_same_label=True):
+    return [
+        np.min([
+            np.max(np.abs(np.array(o1.center) - o2.center))
+            for o1, o2 in itertools.combinations(a.objects, r=2) if count_same_label or o1.label != o2.label
+        ], axis=0)
+        for a in data
+    ]
+
+
+def count_label_occurrences(data):
+    occ = {}
+
+    for a in data:
+        for o in a.objects:
+            if o.label in occ:
+                occ[o.label] += 1
+            else:
+                occ[o.label] = 1
+
+    return occ
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Invalid number of arguments 1 expected:")
+        print(" - directory name")
+        exit(1)
+
+    stats, lab_occ = dir_stats(sys.argv[1])
+
+    print("Statistics:")
+
+    for prop_name, prop_stats in stats.items():
+        print(f'{prop_name}:')
+        for stat_name, stat_value in prop_stats.items():
+            print(f'\t{stat_name}: {stat_value}')
+        print()
+
+    print('Labels occurrences:')
+
+    for lab, occ in lab_occ.items():
+        print(f'\t{lab}: {occ}')
---
+++ whatthefood/data/xml_to_obj.py
@@ -0,0 +1,67 @@
+import os
+import xml.etree.ElementTree as XMLElementTree
+
+
+def parse_dir(dir_name):
+    return [
+        parse_file(os.path.join(dir_name, filename))
+        for filename in os.listdir(dir_name) if os.path.splitext(filename)[1] == '.xml'
+    ]
+
+
+def parse_file(xml_filename):
+    file = XMLElementTree.parse(xml_filename)
+    obj = file.getroot()
+
+    assert obj.tag == 'annotation'
+
+    return Annotation(obj)
+
+
+class Object(object):
+    def __init__(self, xml_obj=None):
+        self.center = None
+        self.size = None
+        self.label = None
+
+        if xml_obj:
+            self.parse(xml_obj)
+
+    def parse(self, xml_obj):
+        bb = xml_obj.find('bndbox')
+        xmin = int(bb.find('xmin').text)
+        xmax = int(bb.find('xmax').text)
+        ymin = int(bb.find('ymin').text)
+        ymax = int(bb.find('ymax').text)
+
+        self.center = (
+            (xmin + xmax) // 2,
+            (ymin + ymax) // 2,
+        )
+        self.size = (
+            xmax - xmin,
+            ymax - ymin,
+        )
+
+        self.label = xml_obj.find('name').text
+
+
+class Annotation(object):
+    def __init__(self, xml_obj=None):
+        self.img_name = None
+        self.img_size = None
+        self.objects = None
+
+        if xml_obj:
+            self.parse(xml_obj)
+
+    def parse(self, xml_obj):
+        self.img_name = xml_obj.find('filename').text
+        size = xml_obj.find('size')
+        self.img_size = (
+            int(size.find('width').text),
+            int(size.find('height').text),
+            int(size.find('depth').text),
+        )
+
+        self.objects = [Object(o) for o in xml_obj.findall('object')]
